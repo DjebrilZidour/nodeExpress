e8cb63aa8ace0ba1cb1a1fd2281f53211b70db4e
Djebril <djebrilacademy@gmail.com>
2025-01-10 00:09:20 +0100

    ty now hope that this is the last good one

--- routes/fetchUsers.js
+++ routes/fetchUsers.js
@@ -2,10 +2,11 @@
 const express = require("express");
 const controllers = require("../controllers/user");
 
+// Create a router instance
 const fetchUsersRouter = express.Router();
 
+// Define the '/users' route on the router instance
+fetchUsersRouter.get('/users', controllers.fetchUsersController);
 
-app.get('/users', controllers.fetchUsersController);
-
-
+// Export the router
 module.exports = fetchUsersRouter;
